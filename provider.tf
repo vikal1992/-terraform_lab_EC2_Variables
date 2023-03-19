7cad6f5954063a8f4c3d08e0d929ca0fb6c6da91
vikal1992 <76895698+vikal1992@users.noreply.github.com>
2023-03-19 12:44:48 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = var.region
-  #access_key = "AKIATNTYWIH5WIAUOICQD"
-  #secret_key = "Comt3p32kbugSZRpTwaXwxf1uYkj4RcruoHtG17"
+  #access_key = "AKIATNTYWI"
+  #secret_key = "Comt3p32kbugSZRpT"
 }
